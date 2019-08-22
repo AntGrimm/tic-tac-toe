d212605faf0b57e48e4e075c12f8333960c0de7f
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-22 11:22:20 -0400

    alternating turns

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 
 export class GameBoard extends Component {
   state = {
-    board: [['X', 'X', 'X'], ['X', 'X', 'X'], ['X', 'X', 'X']]
+    board: [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']],
+    currentPlayer: 'O'
   }
 
   cellClicked = (x, y) => {
     console.log('clicked', x, y)
+    // TODO: toggle the turn
+    const _board = this.state.board
+    _board[x][y] = this.state.currentPlayer
+    const nextPlayer = this.state.currentPlayer === 'O' ? 'X' : 'O'
+    this.setState({
+      board: _board,
+      currentPlayer: nextPlayer
+    })
   }
 
   render() {
@@ -22,7 +31,7 @@ export class GameBoard extends Component {
                     {col.map((row, j) => {
                       return (
                         <td key={j} onClick={() => this.cellClicked(i, j)}>
-                          {i},{j}
+                          {this.state.board[i][j]}
                         </td>
                       )
                     })}
